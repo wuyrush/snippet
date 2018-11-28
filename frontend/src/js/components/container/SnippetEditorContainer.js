a097c5d44c213b9baeaeb376dfedfae8e7ef9c32
doobdoob <wuyrush@gmail.com>
2018-11-28 00:07:46 -0800

    Workable editor
    Restructure frontend so it is using Bloom for better React + Bulma
    interaction. Made my first React editor app working.
    
    TODO:
    * Spacing between the editor panel and the button group in the bottom
    * Use column to separate the buttons so that they can be rearranged in
        vertical on mobile.

--- frontend/src/js/components/container/SnippetEditorContainer.js
+++ frontend/src/js/components/container/SnippetEditorContainer.js
@@ -4,112 +4,190 @@ import ReactDOM from 'react-dom';
 import brace from 'brace';
 import AceEditor from 'react-ace';
 
+// Bloomer for React Bulma boilerplates
+import {
+  Columns,
+  Column,
+  Field,
+  Control,
+  Label,
+  Select,
+  Input,
+  Container,
+  FieldLabel,
+  FieldBody,
+  Checkbox,
+  Button,
+} from 'bloomer';
+
+// TODO: at some point we need to figure out how to dynamically import these modes and themes
+// otherwise the editor page will be loaded with too much unnecessary stuff(though the user might
+// need them later ... We can have browsers cache them so that only the first load is slow - but
+// keep in mind that slow first load can already kill many user's interest:(  )
+
+const languages = [
+  'python',
+  'golang',
+  'rust',
+  'javascript',
+  'markdown',
+];
+
+languages.forEach(lang => {
+  require(`brace/mode/${lang}`);
+});
+
+const themes = [
+  // dark
+  'terminal',
+  'monokai',
+  // bright
+  'github'
+];
+
+themes.forEach(theme => {
+  require(`brace/theme/${theme}`);
+});
+
 class SnippetEditorContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+      snippet_name: '',
+      snippet_text: '',
+      lang: 'python',
+      theme: 'terminal',
+      editorLocked: false,
+    };
+
+    // bind the component instance itself with the function - so in the future when we pass
+    // `this.handleChange` as function object(aka `let someFunc = this.handleChange; someFunc(...)`)
+    // it will always be called with thisValue set to the component instance. This is why we can
+    // pass the functions defined for the parent component down to its children and enable the
+    // parent component to update states whenever children component changes.
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEditorTextChange = this.handleEditorTextChange.bind(this);
   }
 
+  // general purpose handler for inputs like text / select / Checkbox etc. The idea is container
+  // component must have *full* control of the input / data used to render presentational components
+  // associated with it - essentially the ids of widgets which can contain mutable states and their
+  // values. Only by this can the container component know how to map(aka update its states).
+  handleChange(event) {
+    console.log(`handleChange: Got event. [id=${event.target.id}, val=${event.target.value}]`);
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleEditorTextChange(text, event) {
+    this.setState({snippet_text: text});
+  }
   render() {
     return (
       <>
-        <SnippetNameInput value='FakeInput' />
-        <div class="columns">
-          <div class="column is-2">
-            <SelectDropdown options={['python', 'golang', 'rust']} label='Syntax' />
-          </div>
-          <div class="column is-2">
-            <SelectDropdown options={['terminal', 'monokai', 'github']} label='Theme' />
-          </div>
-        </div>
-        <SnippetEditorInput />
-        <div class="field is-grouped">
-          <div class="control">
-            <Button className="button is-link" label="Save" />
-          </div>
-          <div class="control">
-            <Button className="button is-primary" label="Lock" />
-          </div>
-          <div class="control">
-            <Button className="button is-primary" label="Copy to Clipboard" />
-          </div>
-          <div class="control">
-            <Button className="button is-primary" label="Download" />
-          </div>
-        </div>
+        <InputField label='Snippet Name' placeholder='dont-say-i-dont-know' />
+        <Columns isGrid>
+          <Column isSize="narrow">
+            <SelectField
+              options={languages}
+              label='Language' id='lang' value={this.state.lang}
+              handleChange={this.handleChange}
+            />
+          </Column>
+          <Column isSize="narrow">
+            <SelectField
+              options={themes}
+              label='Theme' id='theme' value={this.state.lang}
+              handleChange={this.handleChange}
+            />
+          </Column>
+        </Columns>
+        <SnippetEditorInput
+          lang={this.state.lang}
+          theme={this.state.theme}
+          text={this.state.snippet_text}
+          handleChange={this.handleEditorTextChange}
+        />
+        <Field isGrouped>
+          <Control>
+            <Button isColor="link">Save</Button>
+          </Control>
+          <Control>
+            <Button isColor="primary">Lock</Button>
+          </Control>
+          <Control>
+            <Button isColor="primary">Copy to Clipboard</Button>
+          </Control>
+          <Control>
+            <Button isColor="primary">Download file</Button>
+          </Control>
+        </Field>
       </>
     )
   }
 }
 
-function SnippetNameInput(props) {
+function InputField(props) {
   return (
-    <div class="field">
-      <label class="label">Snippet Name</label>
-      <div class="control">
-        <input class="input is-primary" type="text" value={props.value} placeholder="Snippet name" />
-      </div>
-    </div>
+    <Field>
+      <Label>{props.label}</Label>
+      <Control>
+        <Input isColor="primary" placeholder={props.placeholder} defaultValue={props.defaultValue} />
+      </Control>
+    </Field>
   )
 }
 
-function SelectDropdown(props) {
+function SelectField(props) {
   return (
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">{props.label}</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-          <div class="control">
-            <div class="select is-primary">
-              <select>
-                {
-                  props.options.map(opt => (
-                    <option key={opt} value={opt}>
-                      {opt}
-                    </option>
-                  ))
-                }
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Field isHorizontal>
+      <FieldLabel isNormal>
+        <Label>{props.label}</Label>
+      </FieldLabel>
+      <FieldBody>
+        <Field>
+          <Control>
+            <Select isColor='primary' id={props.id} onChange={props.handleChange}>
+              {
+                props.options.map(opt => (
+                  <option key={opt} value={opt}>{opt}</option>
+                ))
+              }
+            </Select>
+          </Control>
+        </Field>
+      </FieldBody>
+    </Field>
   )
 }
 
-function Checkbox(props) {
+function CheckboxField(props) {
   return (
-    <div class="field">
-      <div class="control">
-        <label class="checkbox">
-          <input type="checkbox" />
-          {props.label}
-        </label>
-      </div>
-    </div>
+    <Field>
+      <Control>
+        <Checkbox>{props.label}</Checkbox>
+      </Control>
+    </Field>
   )
 }
 
 function SnippetEditorInput(props) {
   return (
-    <div class="container">
+    <Container>
       <AceEditor
+        mode={props.lang}
+        theme={props.theme}
+        value={props.text}
+        onChange={props.handleChange}
         tabSize={2}
         highlightActiveLine={true}
         height='500px'
         width='100%'
         fontSize={14}
       />
-    </div>
+    </Container>
   )
-}
-
-function Button(props) {
-  return <a className={props.className}>{props.label}</a>
 }
 
 ReactDOM.render(<SnippetEditorContainer />, document.getElementById('root'));
